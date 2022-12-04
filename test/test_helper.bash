f7817b7c2f09280e148a216b9bcd557cf29f8549
Danny Ben Shitrit <db@dannyben.com>
2022-12-04 06:56:40 +0000

    improve tests format

---
+++ test/test_helper.bash
@@ -0,0 +1,33 @@
+source approvals.bash
+cli=respec
+
+# Stub some commands:
+# - rspec and bundler: for CI, since they are not installed there
+# - bundle and filewatcher: to avoid running them
+rspec()       { printf "[stubbed] \$ rspec %s" "$*"; }
+bundler()     { printf "[stubbed] \$ bundler %s" "$*"; }
+bundle()      { printf "[stubbed] \$ bundle %s" "$*"; }
+filewatcher() { printf "[stubbed] \$ filewatcher %s" "$*"; }
+export -f rspec bundle bundler filewatcher
+
+# Functions for before/after
+set_bad_helper() {
+  # Set up a misconfigured spec_helper
+  export RESPEC_HELPER_PATH="fixtures/misconfigured_spec_helper.rb"
+  export RESPEC_STATUS_FILE="fixtures/spec/status.txt"
+}
+
+set_good_helper() {
+  # Set up a properly configured spec_helper
+  export RESPEC_HELPER_PATH="fixtures/spec_helper.rb"
+  export RESPEC_STATUS_FILE="fixtures/spec/status.txt"
+}
+
+unset_helper() {
+  unset RESPEC_HELPER_PATH
+  unset RESPEC_STATUS_FILE  
+}
+
+# Start clean
+unset_helper
+
