0686b449c92d9f6101ece6857754ba76988ada09
Danny Ben Shitrit <db@dannyben.com>
2022-12-04 06:36:30 +0000

    - Add --watch to continue and fails commands

--- src/fails_command.sh
+++ src/fails_command.sh
@@ -1,2 +1,8 @@
-banner "failed specs"
-run_rspec --next-failure
+watch="${args[--watch]}"
+
+if [[ $watch ]]; then
+  run_watch "respec fails"
+else
+  banner "failed specs"
+  run_rspec --next-failure
+fi
